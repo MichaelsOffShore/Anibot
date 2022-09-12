752438a8d3f0c0bb57b5c1fdb06eda8020fa50e6
Michael <65135168+MichaelsOffShore@users.noreply.github.com>
2022-09-12 21:30:53 +0100

    minor improvements

--- main.js
+++ main.js
@@ -1,16 +1,54 @@
-const discord = require("discord.js");
-const axios = require('axios');
 require("dotenv").config();
 const CoinmarketcapAPI = process.env.CMC;
 const DiscordApi = process.env.DISCORD;
 const CurrencyApi = process.env.CURRENCY;
+const discord = require("discord.js");
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+const imagesDir = 'Images';
+let cwd = process.cwd();
+const OS = process.platform;
 
+if(OS.indexOf("linux") != -1){
+cwd+="/Images/";
+}else{
+cwd+="\\Images\\";
+}
+
+const schedule = require('node-schedule');
 const client = new discord.Client({intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"]});
 
 client.on("ready", () => {
     console.log("Anibot is here, LETS GO!!");
+  
+const job = schedule.scheduleJob('*/5 * * * *', function(){
+  
+  let name = "";
+   fs.readdir(
+    path.resolve(__dirname, imagesDir),
+    (err, files) => {
+  
+      if (err) throw err;
+      name = files[getRandomInt(files.length)];
+      console.log("Attachment: " + cwd+name);
+      console.log("Name: " + name);
+      client.channels.cache.get('1011601421285724220').
+      send({
+        files: [{
+          attachment: cwd+name,
+          name: name,
+          description: 'None'
+        }]
+      })
+        .then()
+        .catch(console.error);
+    }
+  );
+});
   })
   
+  // Cryptocurrency Prices Functionality
   client.on("messageCreate", msg => {
 
     let msgArray = msg.content.toLowerCase().split(" ");
@@ -39,20 +77,13 @@ client.on("ready", () => {
         console.log("Error processing cryptocurrency request");
         console.log(error)
     });
-        // End of discord crypto Function 
       }
     }
   })
-  client.on("messageCreate", msg => {
+   
+   client.on("messageCreate", msg => {
     if (msg.content.toLowerCase() === "!randomphoto") {
 
-      const fs = require('fs');
-      const path = require('path');
-      const imagesDir = 'Images';
-      const cwd = process.cwd()+"/Images/";
-      /* For Windows use cwd below
-      const cwd = process.cwd()+"\\Images\\";
-      */
       let name = "";
        fs.readdir(
         path.resolve(__dirname, imagesDir),
@@ -62,6 +93,7 @@ client.on("ready", () => {
           name = files[getRandomInt(files.length)];
           console.log("Attachment: " + cwd+name);
           console.log("Name: " + name);
+          
           msg.channel.send({
             files: [{
               attachment: cwd+name,
@@ -71,11 +103,9 @@ client.on("ready", () => {
           })
             .then()
             .catch(console.error);
-            // Handle Error
         }
       );
     }
-
 })
     
   client.on("messageCreate", msg => {
@@ -89,14 +119,15 @@ client.on("ready", () => {
     client.off;
     }
   })
-    
 
+  // Get a random number
 function getRandomInt(max) {
   let randNum = Math.floor(Math.random() * max);
   
   return randNum;
 }
 
+//Cryptocurrency Helper Function
 async function getCryptocurrencyPrice(ticker){
   
   // Coin Market Cap Call
@@ -112,7 +143,6 @@ async function getCryptocurrencyPrice(ticker){
   return Math.floor((USDprice) * 100) / 100;
 
 }
-
 
 client.login(DiscordApi);
 
